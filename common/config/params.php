c4f2ef5622d99bec5cea985f7a12e8e7f2bccefd
Yevgeny <https://github.com/DevZhen/lang.git>
2017-09-13 22:56:27 +0300

    modified

--- common/config/params.php
+++ common/config/params.php
@@ -6,7 +6,7 @@ return [
     'urlToImages' => 'http://yiigallery',
 
     /*Путь в файловой системе к альбомам*/
-    'pathToAlbums' => dirname(dirname(__DIR__)) . "/frontend/web/albums",
+    'pathToAlbums' => dirname(dirname(__DIR__)) . "/frontend/web/albums/",
 
     /*Max кол-во альбомов на странице*/
     'count-albums-on-page' => 12,
